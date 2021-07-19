307b94f2a5baf24bad5811bc4f3331e3c1c35d71
Ning Gao <n@gao.us>
2021-07-19 20:55:16 +0800

    Update AppDelegate.swift

--- Prework/AppDelegate.swift
+++ Prework/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        Thread.sleep(forTimeInterval: 1)
         // status bar
         
         UIApplication.shared.theme_setStatusBarStyle([.lightContent, .default, .lightContent, .lightContent], animated: true)
